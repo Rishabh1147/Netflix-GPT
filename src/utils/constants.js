cce37ae4e5c0299b22e546500f2e12d17cdb9358
Rishabh1147 <rishabh111202@gmail.com>
2024-09-30 16:44:29 +0530

    Now Playing Movies API Setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const USERLOGO = "https://occ-0-2483-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7"
 
 export const BACKDROP = "https://assets.nflxext.com/ffe/siteui/vlv3/85ff76db-39e5-423a-afbc-97d3e74db71b/null/IN-en-20240909-TRIFECTA-perspective_b22117e0-4610-4d57-a695-20f77d241a4a_small.jpg"
+
+
+export const API_OPTIONS = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODBjYjk0OWI3Yzg0ZjA2YTU0NjM4MzMzYTg1ZDBkYiIsIm5iZiI6MTcyNzY5MzA1Mi40NTg1NzEsInN1YiI6IjY2ZmE2NTYxNjA2YjU5NWQ5ZmQwMDE5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wxHXKRYBEKo-T60FYXTwEyFvyPccx7JDqA20wIetZ98'
+        }
+};
